6c5fb891d4546e99b7c2648818d6bc831c54dcce
DarkRise12 <darkrisenik@gmail.com>
2021-05-29 13:39:40 +0700

    Add DB class

---
+++ DBFolder/DB.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapApp.DBFolder
+{
+    class DB
+    {
+        MySqlConnection connection = new MySqlConnection("server=localhost;user=prog;password=prog;database=mapapp");
+
+        public void openCon()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+
+        public void closeCon()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Close();
+            }
+        }
+
+        public MySqlConnection getCon()
+        {
+            return connection;
+        }
+    }
+}
